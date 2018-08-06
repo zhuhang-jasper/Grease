fb7ab178e40549c9a6ba9579b944a84342260f53
zhuhang-jasper <zhuhang.jasper@gmail.com>
2018-08-06 16:40:43 +0800

    Update 1.2 - Newly added cards are affected - Add match URL for 'trello.com/c/*'

--- trello.com/trelloLess.user.js
+++ trello.com/trelloLess.user.js
@@ -1,18 +1,72 @@
 // ==UserScript==
 // @name         TrelloLess by zhuhang
 // @namespace    https://greasyfork.org/en/users/200777
-// @version      1.1
+// @version      1.2
 // @description  Remove unwanted Trello cosmetic stuffs
 // @author       zhuhang.jasper
-// @match        https://*.trello.com/b/*
-// @match        https://trello.com/1/boards/*
 // @match        https://trello.com/b/*
-// @match        https://*.trello.com/1/boards/*
+// @match        https://trello.com/c/*
+// @match        https://trello.com/1/boards/*
 // @include      /^https:\/\/.*\.trello\.com\/b\/.*$/
+// @include      /^https:\/\/.*\.trello\.com\/c\/.*$/
 // @grant        none
 // ==/UserScript==
 
-// a function that loads jQuery and calls a callback function when jQuery has finished loading
+// FN001 : Hide colon symbol from custom field badges in cards
+$.fn.hideColonInBadge = function() {
+    $('span .badge-text').each(function(){$(this).html($(this).html().replace(":",""))});
+}
+
+/*** Event Appenders ***/
+$.fn.bindEvents = function() {
+
+    // FN001
+    //$('div.window-wrapper > .dialog-close-button,div.window-wrapper > .js-close-window').off('click').on('click'
+    $('div.window-overlay > div.window').on('hide', function(e) {
+        if ($(this).is(':hidden')) {
+            console.log('TrelloLess-FN001 event triggered');
+            $.fn.hideColonInBadge();
+        }
+    });
+
+}
+
+/*** Startup Events ***/
+$.fn.startUpTrigger = function() {
+
+    // Bind Events On Modal
+    /*$('div.window-overlay > div.window').off('show').on('show', function(e) {
+        //console.log('triggering bindEvents()');
+        $.fn.bindEvents();
+    });*/
+
+    // Add functions on startup here:
+    $.fn.hideColonInBadge(); // FN001
+
+}
+
+/*** Main Body ***/
+function main() {
+    $.fn.bindEvents();
+    $.fn.startUpTrigger();
+}
+
+// Enable CSS show/hide events listener
+// src: https://gist.github.com/kairyou/4aaca43d1528d042fa4c
+(function ($) {
+    $.each(['show', 'hide', 'fadeOut', 'fadeIn'], function (i, ev) {
+        var el = $.fn[ev];
+        $.fn[ev] = function () {
+            var result = el.apply(this, arguments);
+            result.promise().done(function () {
+                this.trigger(ev, [result]);
+            })
+            return result;
+        };
+    });
+})(jQuery);
+
+// A function that loads jQuery and calls a callback function when jQuery has finished loading
 function addJQuery(callback) {
   var script = document.createElement("script");
   script.setAttribute("src", "//code.jquery.com/jquery-1.12.4.min.js");
@@ -22,12 +76,6 @@ function addJQuery(callback) {
     document.body.appendChild(script);
   }, false);
   document.body.appendChild(script);
-}
-
-// Main Logic
-function main() {
-    // Hide Trello colon symbol from custom field badge
-    $('span .badge-text').each(function(){$(this).html($(this).html().replace(":",""))});
 }
 
 // Script jQuery Wrapper
